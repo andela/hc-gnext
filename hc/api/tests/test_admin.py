7dfa3e1ee20b6c927e123a9dd363412dd70d3ab4
silverjimmy <silverjimmy2@gmail.com>
2017-12-05 00:31:52 +0300

    [Chore#153114590] issued that test match PEP8 standard

--- hc/api/tests/test_admin.py
+++ hc/api/tests/test_admin.py
@@ -4,7 +4,10 @@ from hc.api.transports import Pushbullet
 
 
 class ApiAdminTestCase(BaseTestCase):
-
+    '''
+    This checks a user and sets the user to be staff as well as superuser.
+    
+    '''
     def setUp(self):
         super(ApiAdminTestCase, self).setUp()
         self.check = Check.objects.create(user=self.alice, tags="foo bar")
@@ -14,6 +17,9 @@ class ApiAdminTestCase(BaseTestCase):
         # Set Alice to be staff and superuser and save her :)
 
     def test_it_shows_channel_list_with_pushbullet(self):
+        '''
+        Shows channel list with pushbullet
+        '''
         self.client.login(username="alice@example.org", password="password")
         ch = Channel(user=self.alice, kind="pushbullet", value="test-token")
         ch.save()
