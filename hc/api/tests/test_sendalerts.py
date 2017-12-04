09886bdaa694f6e879d71e3e05cede8cf48fade8
silverjimmy <silverjimmy2@gmail.com>
2017-12-04 10:52:41 +0300

    [Chore#153114590] Updated missing and failing asserts to pass

--- hc/api/tests/test_sendalerts.py
+++ hc/api/tests/test_sendalerts.py
@@ -11,23 +11,24 @@ class SendAlertsTestCase(BaseTestCase):
 
     @patch("hc.api.management.commands.sendalerts.Command.handle_one")
     def test_it_handles_few(self, mock):
-        yesterday = timezone.now() - timedelta(days=1)
+        yesterday = self.now - timedelta(days=1)
         names = ["Check %d" % d for d in range(0, 10)]
 
         for name in names:
             check = Check(user=self.alice, name=name)
             check.alert_after = yesterday
             check.status = "up"
+            check.last_ping = timezone.now()
             check.save()
 
         result = Command().handle_many()
-        assert result, "handle_many should return True"
+        self.assertTrue(result)
 
         handled_names = []
         for args, kwargs in mock.call_args_list:
             handled_names.append(args[0].name)
 
-        assert set(names) == set(handled_names)
+        self.assertSetEqual(set(names), set(handled_names))
         ### The above assert fails. Make it pass
 
     def test_it_handles_grace_period(self):
@@ -37,6 +38,7 @@ class SendAlertsTestCase(BaseTestCase):
         check.save()
 
         # Expect no exceptions--
-        Command().handle_one(check)
+        r = Command().handle_one(check)
+        self.assertTrue(r)
 
     ### Assert when Command's handle many that when handle_many should return True
