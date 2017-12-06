feef0600892fe4d71b79484b8fbe0c321287c111
geofrocker <geofrocker2@gmail.com>
2017-12-06 12:02:48 +0300

    Chore/Update test check models

--- hc/api/tests/test_check_model.py
+++ hc/api/tests/test_check_model.py
@@ -12,9 +12,11 @@ class CheckModelTestCase(TestCase):
 
         check.tags = " foo  bar "
         self.assertEquals(check.tags_list(), ["foo", "bar"])
+
+        # Repeat above test for when check is an empty string
         check.tags = ""
         self.assertEquals(check.tags_list(), [])
-        # Repeat above test for when check is an empty string
+       
 
     def test_status_works_with_grace_period(self):
         check = Check()
@@ -37,8 +39,9 @@ class CheckModelTestCase(TestCase):
         check.status = "paused"
         self.assertFalse(check.in_grace_period())
 
+        # Test that when a new check is created, it is not in the grace periodd
         check.status = "new"
         check.last_ping = timezone.now()
         self.assertFalse(check.in_grace_period())
 
-    # Test that when a new check is created, it is not in the grace periodd
+    
