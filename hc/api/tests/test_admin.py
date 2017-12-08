64db0afd17a56dd5f60ba8447816b0f87d84a557
geofrocker <geofrocker2@gmail.com>
2017-12-08 13:27:32 +0300

    [Chore#153114590] Rename rsp to a meaningful name

--- hc/api/tests/test_admin.py
+++ hc/api/tests/test_admin.py
@@ -11,10 +11,12 @@ class ApiAdminTestCase(BaseTestCase):
     def setUp(self):
         super(ApiAdminTestCase, self).setUp()
         self.check = Check.objects.create(user=self.alice, tags="foo bar")
+        
+        # Set Alice to be staff and superuser and save her :)
         self.alice.is_staff = True
         self.alice.is_superuser = True
         self.alice.save()
-        # Set Alice to be staff and superuser and save her :)
+        
 
     def test_it_shows_channel_list_with_pushbullet(self):
         '''
@@ -23,5 +25,7 @@ class ApiAdminTestCase(BaseTestCase):
         self.client.login(username="alice@example.org", password="password")
         ch = Channel(user=self.alice, kind="pushbullet", value="test-token")
         ch.save()
-        rsp = ch.notify(self.check)
-        self.assertIn('401', rsp)
+
+        #Assert for pushbullet
+        response = ch.notify(self.check)
+        self.assertIn('401', response)
