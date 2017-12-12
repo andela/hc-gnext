6c1113a2eca353747f3c1bcfe82ccc81b5d77529
silverjimmy <silverjimmy2@gmail.com>
2017-12-12 11:58:25 +0300

    Initial commit of work

--- hc/settings.py
+++ hc/settings.py
@@ -83,8 +83,8 @@ TEST_RUNNER = 'hc.api.tests.CustomRunner'
 # install requirements.txt and do manage.py runserver and it works
 DATABASES = {
     'default': dj_database_url.config()
+    
     }
-}
 
 # You can switch database engine to postgres or mysql using environment
 # variable 'DB'. Travis CI does this.
