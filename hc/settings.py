51cfef894a83961a7f31873978ed03cd20119a25
silverjimmy <silverjimmy2@gmail.com>
2017-12-12 19:13:09 +0300

    changed Site Root url to point to new server

--- hc/settings.py
+++ hc/settings.py
@@ -118,7 +118,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-SITE_ROOT = "https://healthchck.herokuapp.com/"
+SITE_ROOT = "https://healthchck.herokuapp.com"
 PING_ENDPOINT = SITE_ROOT + "/ping/"
 PING_EMAIL_DOMAIN = HOST
 STATIC_URL = '/static/'
