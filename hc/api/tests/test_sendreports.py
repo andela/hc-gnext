4855f402340da31a6d37d2b3606b1c9089699196
flacode <flavianshemerirwe@gmail.com>
2017-12-12 16:16:52 +0300

    [Feature #153114592] Fix pep8 style issues

--- hc/api/tests/test_sendreports.py
+++ hc/api/tests/test_sendreports.py
@@ -5,15 +5,27 @@ from hc.test import BaseTestCase
 from freezegun import freeze_time
 from unittest import mock
 
+
 class SendReportsTestCase(BaseTestCase):
+
+    def setUp(self):
+        super(SendReportsTestCase, self).setUp()
+        self.alice.date_joined = datetime.datetime(
+            2017, 1, 11, 5, 34, 3
+            )
+        self.alice.save()
 
     @mock.patch("hc.api.management.commands.sendreports.num_pinged_checks")
     @freeze_time("2018-01-12 06:33:24", tz_offset=+0)
     def test_handle_month_run_sends_email(self, mock_pings):
-        self.alice.date_joined = datetime.datetime(2017, 1, 11, 5, 34, 3)
-        self.alice.save()
+        """
+        Test a user who subscribed to monthly reports
+        receives a report at the next report date
+        """
         self.profile.reports_duration = 30
-        self.profile.next_report_date = datetime.datetime(2018, 1, 12, 5, 34, 3)
+        self.profile.next_report_date = datetime.datetime(
+            2018, 1, 12, 5, 34, 3
+            )
         self.profile.save()
         mock_pings.return_value = 1
         result = Command().handle_month_run()
@@ -22,10 +34,14 @@ class SendReportsTestCase(BaseTestCase):
     @mock.patch("hc.api.management.commands.sendreports.num_pinged_checks")
     @freeze_time("2017-12-30 06:33:24", tz_offset=+0)
     def test_handle_month_run_does_not_send_email(self, mock_pings):
-        self.alice.date_joined = datetime.datetime(2017, 1, 11, 5, 34, 3)
-        self.alice.save()
+        """
+        Test a user who subscribed to monthly reports does not receive
+        a report until the next report date
+        """
         self.profile.reports_duration = 30
-        self.profile.next_report_date = datetime.datetime(2018, 1, 12, 5, 34, 3)
+        self.profile.next_report_date = datetime.datetime(
+            2018, 1, 12, 5, 34, 3
+            )
         self.profile.save()
         mock_pings.return_value = 1
         result = Command().handle_month_run()
@@ -34,10 +50,14 @@ class SendReportsTestCase(BaseTestCase):
     @mock.patch("hc.api.management.commands.sendreports.num_pinged_checks")
     @freeze_time("2017-12-19 06:33:24", tz_offset=+0)
     def test_handle_week_run_sends_email(self, mock_pings):
-        self.alice.date_joined = datetime.datetime(2017, 1, 11, 5, 34, 3)
-        self.alice.save()
+        """
+        Test a user who subscribed to weekly reports receives
+        a report at the next report date
+        """
         self.profile.reports_duration = 7
-        self.profile.next_report_date = datetime.datetime(2017, 12, 19, 5, 34, 3)
+        self.profile.next_report_date = datetime.datetime(
+            2017, 12, 19, 5, 34, 3
+            )
         self.profile.save()
         mock_pings.return_value = 1
         result = Command().handle_week_run()
@@ -46,10 +66,14 @@ class SendReportsTestCase(BaseTestCase):
     @mock.patch("hc.api.management.commands.sendreports.num_pinged_checks")
     @freeze_time("2017-12-14 06:33:24", tz_offset=+0)
     def test_handle_week_run_does_not_send_email(self, mock_pings):
-        self.alice.date_joined = datetime.datetime(2017, 1, 11, 5, 34, 3)
-        self.alice.save()
+        """
+        Test a user who subscribed to weekly reports does not receive
+        a report until the next report date
+        """
         self.profile.reports_duration = 7
-        self.profile.next_report_date = datetime.datetime(2017, 12, 19, 5, 34, 3)
+        self.profile.next_report_date = datetime.datetime(
+            2017, 12, 19, 5, 34, 3
+            )
         self.profile.save()
         mock_pings.return_value = 1
         result = Command().handle_week_run()
@@ -57,11 +81,15 @@ class SendReportsTestCase(BaseTestCase):
 
     @mock.patch("hc.api.management.commands.sendreports.num_pinged_checks")
     @freeze_time("2017-12-13 06:33:24", tz_offset=+0)
-    def test_daily_week_run_sends_email(self, mock_pings):
-        self.alice.date_joined = datetime.datetime(2017, 1, 11, 5, 34, 3)
-        self.alice.save()
+    def test_daily_run_sends_email(self, mock_pings):
+        """
+        Test a user who subscribed to daily reports receives
+        a report at the next report date
+        """
         self.profile.reports_duration = 1
-        self.profile.next_report_date = datetime.datetime(2017, 12, 13, 5, 34, 3)
+        self.profile.next_report_date = datetime.datetime(
+            2017, 12, 13, 5, 34, 3
+            )
         self.profile.save()
         mock_pings.return_value = 1
         result = Command().handle_daily_run()
@@ -69,14 +97,16 @@ class SendReportsTestCase(BaseTestCase):
 
     @mock.patch("hc.api.management.commands.sendreports.num_pinged_checks")
     @freeze_time("2017-12-12 06:33:24", tz_offset=+0)
-    def test_daily_week_run_does_not_send_email(self, mock_pings):
-        self.alice.date_joined = datetime.datetime(2017, 1, 11, 5, 34, 3)
-        self.alice.save()
+    def test_daily_run_does_not_send_email(self, mock_pings):
+        """
+        Test a user who subscribed to daily reports does not receive
+        a report until the next report date
+        """
         self.profile.reports_duration = 1
-        self.profile.next_report_date = datetime.datetime(2017, 12, 13, 5, 34, 3)
+        self.profile.next_report_date = datetime.datetime(
+            2017, 12, 13, 5, 34, 3
+            )
         self.profile.save()
         mock_pings.return_value = 1
         result = Command().handle_daily_run()
         self.assertEqual(result, 0)
-
-
