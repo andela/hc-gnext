82c94b353a2bea15cbd0bdfae4689ca8eb486a06
geofrocker <geofrocker2@gmail.com>
2017-12-21 16:17:49 +0300

    [Chore #153114593] Solve indentation errors

--- hc/api/models.py
+++ hc/api/models.py
@@ -112,12 +112,12 @@ class Check(models.Model):
         return up_ends < timezone.now() < grace_ends
      
     def in_reverse_grace(self):
-		if self.status in ("new", "paused"):
-			return False
+        if self.status in ("new", "paused"):
+            return False
 
-		up_ends = self.last_ping + self.timeout
-		grace_begins = up_ends - self.grace
-		return grace_begins < timezone.now() < up_ends
+        up_ends = self.last_ping + self.timeout
+        grace_begins = up_ends - self.grace
+        return grace_begins < timezone.now() < up_ends
 
     def assign_all_channels(self):
         if self.user:
