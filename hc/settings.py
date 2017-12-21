886d0b801f995fe6be71b2a52f885ad44b5af7e6
silverjimmy <silverjimmy2@gmail.com>
2017-12-21 16:24:40 +0300

    [feature #153114592] Correct files having pylint and pep8 issues.

--- hc/settings.py
+++ hc/settings.py
@@ -144,7 +144,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')  # your email
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')  # your email
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')  # your password
 
 EMAIL_USE_TLS = True
