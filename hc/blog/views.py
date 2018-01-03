99ba0fe8666b51b9fcc400589cd035f13af9ceef
Gideon Kimutai <gideonkimutai9@gmail.com>
2018-01-03 09:44:00 +0300

    [Feature #153114603]Add create category view

--- hc/blog/views.py
+++ hc/blog/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.core.urlresolvers import reverse
+from django.views.generic import CreateView
+from .forms import CategoryForm
+from .models import Category
 
-# Create your views here.
+
+class CreateCategoryView(CreateView):
+    form_class = CategoryForm
+    template_name = 'blog/category_create.html'
+
+    def get_success_url(self):
+        return reverse('hc-blog:category-create')
+
+    def get_context_data(self, **kwargs):
+        ctx = super(CreateCategoryView, self).get_context_data(**kwargs)
+        categories = Category.objects.all()
+        ctx['categories'] = categories
+        return ctx
