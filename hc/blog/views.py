508c68ab3cc5177347ec1433fae313dd13989e4c
baron <baron@barons-MacBook-Pro.local>
2018-01-04 11:58:19 +0300

    [Feature #153114603]Render blog post form to handle blog post creation

--- hc/blog/views.py
+++ hc/blog/views.py
@@ -1,6 +1,6 @@
 from django.core.urlresolvers import reverse
 from django.views.generic import CreateView, TemplateView
-from .forms import CategoryForm
+from .forms import CategoryForm, PostForm
 from .models import Category
 
 
@@ -17,7 +17,9 @@ class CreateCategoryView(CreateView):
 
     def get_context_data(self, **kwargs):
         ctx = super(CreateCategoryView, self).get_context_data(**kwargs)
+        post_form = PostForm()
         categories = Category.objects.all()
         ctx['categories'] = categories
+        ctx['post_form'] = post_form
         return ctx
 
