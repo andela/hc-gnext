14fe6498dfa83b3b8993b894e579634ac1651328
Gideon Kimutai <gideonkimutai9@gmail.com>
2018-01-04 13:04:48 +0300

    [Feature #153114603]Add test for view blog post content

--- hc/blog/tests/test_blog_post.py
+++ hc/blog/tests/test_blog_post.py
@@ -23,6 +23,13 @@ class BlogPostTestCase(BaseTestCase):
         category.save()
         self.category = category
 
+        # blog post form.
+        self.post_title = 'test title'
+        self.blog_post_form = dict(
+            categories=[self.category.id, ],
+            title=self.post_title,
+            content='this is a blog post test content')
+
         self.create_blog_post_link = reverse('hc-blog:post-create')
 
     def test_user_can_create_blog_post(self):
@@ -30,22 +37,31 @@ class BlogPostTestCase(BaseTestCase):
         User should be able to create blog post content.
         """
 
-        # blog post form.
-        post_title = 'test title'
-        blog_post_form = dict(
-            categories=[self.category.id, ],
-            title=post_title,
-            content='this is a blog post test content')
-
         # create post content.
-        response = self.client.post(self.create_blog_post_link, blog_post_form)
+        response = self.client.post(self.create_blog_post_link, self.blog_post_form)
 
         # filter posts using title in blog_post_form.
-        posts = Post.objects.filter(title=post_title)
+        posts = Post.objects.filter(title=self.post_title)
 
         # assertions.
         self.assertEqual(response.status_code, 302)
         self.assertGreater(posts.count(), 0)
-        self.assertEqual(posts.first().title, post_title)
+        self.assertEqual(posts.first().title, self.post_title)
         self.assertEqual(posts.first().categories.count(), 1)
         assert posts.first().categories.first() == self.category
+
+    def test_user_can_view_blog_post(self):
+        """
+        A user should be able to view a single blog post entry.
+        """
+
+        self.client.post(self.create_blog_post_link, self.blog_post_form)
+        post = Post.objects.get(name=self.blog_post_form.get('title'))
+        blog_post_detail_url = reverse('hc-blog:blog-detail', kwargs={'slug': post.slug})
+
+        view_response = self.client.get(blog_post_detail_url)
+
+        view_context = view_response.context()
+
+        assert view_response.status_code == 200
+        self.assertIn(post, view_context)
