0d53f66f85e0581e22fec209ac08420d844644d5
Gideon Kimutai <gideonkimutai9@gmail.com>
2018-01-04 22:57:19 +0300

    [Feature #153114603]Add check for post owners and authentication

--- hc/blog/views.py
+++ hc/blog/views.py
@@ -1,20 +1,15 @@
+from django.contrib import messages
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from django.views import generic
 from .forms import CategoryForm, PostForm
 from .models import Category, Post
+from .mixins import CommonContentMixin
+
+from braces.views import UserPassesTestMixin, LoginRequiredMixin
 
 
-class CommonContentMixin(object):
-    def get_context_data(self, **kwargs):
-        ctx = super(CommonContentMixin, self).get_context_data(**kwargs)
-        previous_url = self.request.META.get('HTTP_REFERER', None)
-        if previous_url:
-            ctx['previous_url'] = previous_url
-        return ctx
-
-
-class BlogIndexView(CommonContentMixin, generic.TemplateView):
+class BlogIndexView(LoginRequiredMixin, CommonContentMixin, generic.TemplateView):
     template_name = 'blog/index.html'
 
     def get_context_data(self, **kwargs):
@@ -32,11 +27,12 @@ class BlogIndexView(CommonContentMixin, generic.TemplateView):
         return ctx
 
 
-class CreateCategoryView(CommonContentMixin, generic.CreateView):
+class CreateCategoryView(LoginRequiredMixin, CommonContentMixin, generic.CreateView):
     form_class = CategoryForm
     template_name = 'blog/post_form.html'
 
     def get_success_url(self):
+        messages.success(self.request, 'Blog post category created')
         return reverse('hc-blog:category-create')
 
     def get_context_data(self, **kwargs):
@@ -49,32 +45,50 @@ class CreateCategoryView(CommonContentMixin, generic.CreateView):
         return ctx
 
 
-class CreateBlogPostView(CommonContentMixin, generic.CreateView):
+class CreateBlogPostView(LoginRequiredMixin, CommonContentMixin, generic.CreateView):
     form_class = PostForm
     template_name = 'blog/post_form.html'
 
     def get_success_url(self):
+        messages.success(self.request, 'Blog post created')
         return reverse('hc-blog:index')
 
     def get(self, request, *args, **kwargs):
+        """
+        Redirect all GET requests to the view
+        """
+
         super(CreateBlogPostView, self).get(request, *args, **kwargs)
         return HttpResponseRedirect(reverse('hc-blog:category-create'))
 
     def get_form_kwargs(self):
         kwargs = super(CreateBlogPostView, self).get_form_kwargs()
-        if not 'request' in kwargs:
+        if 'request' not in kwargs:
             kwargs.update({'request': self.request})
 
         return kwargs
 
 
-class RetrievePostDetailView(CommonContentMixin, generic.DetailView):
+class RetrievePostDetailView(LoginRequiredMixin, CommonContentMixin, generic.DetailView):
     model = Post
 
 
-class UpdatePostView(generic.UpdateView):
+class UpdatePostView(
+    LoginRequiredMixin,
+    UserPassesTestMixin,
+    CommonContentMixin,
+    generic.UpdateView):
     model = Post
     form_class = PostForm
+
+    def test_func(self, user):
+        slug = self.kwargs.get('slug', None)
+        if slug:
+            obj = Post.objects.filter(slug=slug)
+            if obj.exists():
+                owner = obj.first().created_by == user
+                return owner if True else messages.warning(self.request, 'You are not the owner of the blog post!')
+        return False
 
     def get_success_url(self):
         return reverse('hc-blog:post-detail', kwargs={'slug': self.object.slug})
@@ -93,8 +107,17 @@ class UpdatePostView(generic.UpdateView):
         return ctx
 
 
-class DeletePostView(CommonContentMixin, generic.DeleteView):
+class DeletePostView(LoginRequiredMixin, UserPassesTestMixin, CommonContentMixin, generic.DeleteView):
     model = Post
+
+    def test_func(self, user):
+        slug = self.kwargs.get('slug', None)
+        if slug:
+            obj = Post.objects.filter(slug=slug)
+            if obj.exists():
+                owner = obj.first().created_by == user
+                return owner if True else messages.warning(self.request, 'You are not the owner of the blog post!')
+        return False
 
     def post(self, request, *args, **kwargs):
         post_data = request.POST
@@ -107,4 +130,5 @@ class DeletePostView(CommonContentMixin, generic.DeleteView):
         return super(DeletePostView, self).post(request, *args, **kwargs)
 
     def get_success_url(self):
+        messages.success(self.request, 'Blog post succesfully deleted')
         return reverse('hc-blog:index')
