6cbb4b8690650f63178ea40c07fa029288a8a00c
Gideon Kimutai <gideonkimutai9@gmail.com>
2018-01-07 07:54:28 +0300

    [Feature #153114603]Fix urls regex match to include digits and underscores

--- hc/blog/urls.py
+++ hc/blog/urls.py
@@ -17,15 +17,15 @@ urlpatterns = [
         views.BlogPostCreateView.as_view(),
         name='post-create'),
 
-    url(r'^post/(?P<slug>[a-zA-Z0-9-]+)$',
+    url(r'^post/(?P<slug>[a-zA-Z0-9_-]+)$',
         views.BlogPostDetailView.as_view(),
         name='post-detail'),
 
-    url(r'^post/(?P<slug>[a-zA-Z0-9-]+)/edit/$',
+    url(r'^post/(?P<slug>[a-zA-Z0-9_-]+)/edit/$',
         views.BlogPostUpdateView.as_view(),
         name='post-update'),
 
-    url(r'^post/(?P<slug>[a-zA-Z0-9-]+)/delete/$',
+    url(r'^post/(?P<slug>[a-zA-Z0-9_-]+)/delete/$',
         views.BlogPostDeleteView.as_view(),
         name='post-delete'),
 ]
